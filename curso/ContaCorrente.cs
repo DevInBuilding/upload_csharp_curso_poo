0495891fbaf87712989531c47d5f5b5ff4634bb2
Lucas Coelho Barros <lucasbarrosprofessional@gmail.com>
2023-02-08 23:04:40 -0300

    Adicionar arquivos de projeto.

---
+++ curso/ContaCorrente.cs
@@ -0,0 +1,63 @@
+﻿using bytebank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public bytebank.Titular.Cliente titular;
+        public string nome_agencia;
+        public int numero_agencia;
+        public string conta;
+        public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+
+    }
+}
---
+++ curso/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bytebank;
+
+
+Console.WriteLine("Boas Vindas ao seu banco, ByteBank!\n");
+//ContaCorrente conta1 = new ContaCorrente();
+//conta1.titular = "André Silva";
+//conta1.conta = "110123-X";
+//conta1.numero_agencia = 123;
+//conta1.nome_agencia = "Agência Central";
+//conta1.saldo = 100;
+
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.titular = "Amanda Nogueira";
+//conta2.conta = "10106-X";
+//conta2.numero_agencia = 321;
+//conta2.nome_agencia = "Agência Central";
+//conta2.saldo = 100;
+
+//Console.WriteLine("Saldo da Amanda pré-transferência: " + conta2.saldo);
+//Console.WriteLine("Saldo do André  pré- Transferência: " + conta1.saldo);
+//bool transferencia = conta1.Transferir(50, conta2);
+//Console.WriteLine("Transferência realizada com sucesso? " + transferencia);
+//Console.WriteLine("Saldo da Amanda pós-transferência: " + conta2.saldo);
+//Console.WriteLine("Saldo do André  pós- Transferência: " + conta1.saldo);
+
+bytebank.Titular.Cliente cliente = new bytebank.Titular.Cliente();
+cliente.nome = "André Silva";
+cliente.cpf = "546.321.789-33";
+cliente.profissao = "Gerente de Vendas";
+
+Console.WriteLine();
